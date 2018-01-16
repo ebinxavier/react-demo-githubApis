43abffd93fbd158a93b42d95b6b2c09b3a2c7de0
Xavier <548500@cognizant.com>
2018-01-16 14:08:48 +0530

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Button ,FormControl,FormGroup,Grid,Col,Row,Glyphicon} from 'react-bootstrap';
 import './App.css';
 
-const Card = (props)=>{
-  const repoLimit=10;
-  return	(
-  	<div style={{border:"2px solid #eee",borderRadius:"10px"}}>
-    <img style={{padding:'10px',marginTop: '-50px',borderRadius:'100px',width:'100px'}} src={props.url}/>
-      <div style={{display:'inline-block',padding:'10px'}}>
-				<p style={{fontSize:"30px",fontWeight:"bold"}}>{props.name}</p>
-        <p style={{fontStyle:"italic"}}>{props.company}</p>
-        <p style={{fontSize:"20px",fontWeight:"bold"}}>Repositories</p>
-          <ul>
-            {props.repos.slice(0,repoLimit).map((data)=>{
-              return <li key={data}>{data}</li>
-              })
-              }
-              {/*conditional rendering*/}
-              {props.repos.length>repoLimit &&
-                  <li>....</li>
-              }
 
-          </ul>
-      </div>
-    </div>
+const Header = (props)=>{
+  return(
+    <h1 style={{textAlign:'center'}}>Find on GitHub</h1>
+
   )
+}
+
+class Card extends Component{
+
+  repoLimit=10;
+  deleteCard = (name)=>{
+    this.props.removeUser(this.props.name)
+  }
+
+ render(props){
+  return	(
+  	
+      <Grid style={{border:"2px solid #eee",borderRadius:"10px",padding:'25px',marginTop: "10px"}}>
+        <Row> 
+              <Button style={{float:"right"}} bsStyle="danger" onClick={this.deleteCard}><Glyphicon glyph="trash" /></Button>
+              <Col xs={12} md={4}>
+                    <img style={{borderRadius:'100px',width:'200px'}} src={this.props.url}/>
+                    <p style={{fontSize:"30px",fontWeight:"bold"}}>{this.props.name}</p>
+                    <p style={{fontStyle:"italic"}}>{this.props.company}</p>        
+              </Col>
+
+               
+              <Col xs={12} md={8} style={{display:'inline-block',padding:'10px'}}>
+                <p style={{fontSize:"20px",fontWeight:"bold"}}>Repositories</p>
+                  <ul>
+                    {this.props.repos.slice(0,this.repoLimit).map((data)=>{
+                      return <li key={data}>{data}</li>
+                      })
+                      }
+                      {/*conditional rendering*/}
+                      {this.props.repos.length>this.repoLimit &&
+                          <li>....</li>
+                      }
+
+                  </ul>
+              </Col>
+        </Row>
+      </Grid>
+  )
+}
 }
 
 const CardList = (props)=>{
   return(
     <div>
       {props.data.map((data)=>{
-      return <Card key={data.name} {...data}/>
+      return <Card key={data.name} {...data} removeUser={props.removeUser}/>
       })}
       
     </div>
@@ -46,12 +70,22 @@ class SearchBox extends Component{
   state = {user:""}
   render(){
     return (
-      <div>
-        <input type="text" placeholder="Add user" onChange={(event)=>{         
-          this.setState({user:event.target.value})
-        }}/>
-        <button onClick={this.handleClick}>Click Me</button>
-      </div>
+      <FormGroup>
+        <Grid style={{marginTop:"20px"}}>
+          <Row className="show-grid">
+            <Col xs={10}>
+                  <FormControl type="text" placeholder="Add user" onChange={(event)=>{         
+                          this.setState({user:event.target.value})
+                          }}/>
+            </Col>
+
+            <Col xs={2}>
+            <Button bsStyle="primary" onClick={this.handleClick}>Search User</Button>
+            </Col>
+          </Row>
+        </Grid>
+      </FormGroup>
+       
     )
   }
 
@@ -97,11 +131,26 @@ axios.get('https://api.github.com/users/'+user)
 }).catch(err=>{console.log(err)})
 
     }
+
+   removeUser = (name)=>{
+     console.log(name);
+        for(var i in this.state.data){
+          if(this.state.data[i].name == name){
+            this.state.data.splice(i,1);
+            console.log(this.state.data);
+            this.setState({data:this.state.data});
+          }
+        }
+   } 
   render() {
     return (
       <div>
-       <CardList data={this.state.data}/>
+        <div style={{position: "fixed",zIndex: "1",background: "#c6cccc",width: "100%"}}>
+        <Header/>
        <SearchBox addUser={this.addUser}/>
+       </div>
+       <div style={{height: "128px"}}></div>
+       <CardList data={this.state.data} removeUser={this.removeUser}/>
       </div>
     );
   }
